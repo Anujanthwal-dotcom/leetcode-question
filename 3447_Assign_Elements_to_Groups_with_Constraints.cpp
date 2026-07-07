00266c90c1eb5827781bd1683b260e8515a6e220
Anuj Anthwal <152400521+Anujanthwal-dotcom@users.noreply.github.com>
2026-07-07 19:39:07 +0530

    Sync: 3447. Assign Elements to Groups with Constraints (Medium) - Leet2Git

---
+++ 3447_Assign_Elements_to_Groups_with_Constraints.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> assignElements(vector<int>& groups, vector<int>& elements) {
+        int n = groups.size();
+        
+        vector<int> assigned(n,-1);
+
+        unordered_map<int,int> map;
+
+        for(int i = 0; i<elements.size();i++){
+            map[elements[i]] = i;
+        }
+
+        for(int i = 0;i<n;i++){
+            for(int x = 1;x*x<=groups[i];x++){
+                if(groups[i] % x == 0){
+                    int first = x;
+                    int second = groups[i] / x;
+
+                    if(map.count(first)){
+                        if(assigned[i] == -1){
+                            assigned[i] = map[first];
+                        }
+                        else{
+                            assigned[i] = min(assigned[i] , map[first]);
+                        }
+                    }
+
+                    if(map.count(second)){
+                        if(assigned[i] == -1){
+                            assigned[i] = map[second];
+                        }
+                        else{
+                            assigned[i] = min(assigned[i] , map[second]);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return assigned;
+    }
+};
