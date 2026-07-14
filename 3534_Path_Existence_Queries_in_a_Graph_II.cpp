a8ac5aa3f078a4da4e14e9726c51ea6dc568bdfe
Anuj Anthwal <152400521+Anujanthwal-dotcom@users.noreply.github.com>
2026-07-14 08:37:56 +0530

    Sync: 3534. Path Existence Queries in a Graph II (Hard) - Leet2Git

--- 3534_Path_Existence_Queries_in_a_Graph_II.cpp
+++ 3534_Path_Existence_Queries_in_a_Graph_II.cpp
@@ -128,3 +128,72 @@
 //
 // 	  * 0 <= ui, vi < n
 
+class Solution {
+public:
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<pair<int,int>> newNums(n);
+        // making pairs
+        for(int i = 0;i<n;i++){
+            newNums[i] = {nums[i],i};
+        }
+        //sorting
+        sort(newNums.begin(),newNums.end());
+
+        //index translation
+        vector<int> getI(n);
+
+        for(int i = 0;i<n;i++){
+            getI[newNums[i].second] = i;
+        }
+
+        //  build st[i][j] = farthest index reachable from i in 2^j jumps.
+        vector<vector<int>> st(n,vector<int>(18));
+
+        int r = 0;
+
+        for(int i = 0;i<n;i++){
+            if(r<i) r = i;
+
+            while(r+1<n && 
+                newNums[r+1].first - newNums[r].first <=maxDiff 
+                && 
+                newNums[r+1].first - newNums[i].first<=maxDiff
+            ){
+                r++;
+            }
+
+            st[i][0] = r;
+        }
+
+        for(int j = 1;j<18;j++){
+            for(int i =0;i<n;i++){
+                st[i][j] = st[st[i][j-1]][j-1];
+            }
+        }
+
+
+        //queries
+        vector<int> ans(queries.size(),-1);
+
+        for(int i = 0;i<(int) queries.size();i++){
+            int a = getI[queries[i][0]];
+            int b = getI[queries[i][1]];
+
+            if(a>b) swap(a,b);
+
+            if(a==b) {ans[i] = 0;continue;}
+
+            int curr = a,steps = 0;
+            for(int j = 17;j>=0;j--){
+                if(st[curr][j]<b) {
+                    curr = st[curr][j];
+                    steps += (1<<j);
+                }
+            }
+
+            ans[i] = (st[curr][0]>=b)? steps+1:-1;
+        }
+
+        return ans;
+    }
+};
