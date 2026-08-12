4d10dea4787092c72188d546932555b51c9974ef
Anuj Anthwal <152400521+Anujanthwal-dotcom@users.noreply.github.com>
2026-08-12 16:29:41 +0530

    Sync: 516. Longest Palindromic Subsequence (Medium) - Leet2Git

--- 516_Longest_Palindromic_Subsequence.cpp
+++ 516_Longest_Palindromic_Subsequence.cpp
@@ -57,6 +57,13 @@ public:
             }
         }
 
+        for(int i = 0;i<=n;i++){
+            for(int j = 0;j<=n;j++){
+                cout<<dp[i][j]<<" "<<endl;
+            }
+            cout<<endl;
+        }
+
         return dp[n][n];
     }
 };
