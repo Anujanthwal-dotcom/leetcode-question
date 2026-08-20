405cc285a35fb2bb93c5b380832786390d2942d6
Anuj Anthwal <152400521+Anujanthwal-dotcom@users.noreply.github.com>
2026-08-20 19:28:51 +0530

    Sync: 1094. Car Pooling (Medium) - Leet2Git

---
+++ 1094_Car_Pooling.cpp
@@ -0,0 +1,57 @@
+// 1094. Car Pooling
+// Difficulty: Medium
+// URL: https://leetcode.com/problems/car-pooling/
+//
+// There is a car with capacity empty seats. The vehicle only drives east (i.e., it cannot turn around and drive west).
+//
+// You are given the integer capacity and an array trips where trips[i] = [numPassengersi, fromi, toi] indicates that the ith trip has numPassengersi passengers and the locations to pick them up and drop them off are fromi and toi respectively. The locations are given as the number of kilometers due east from the car's initial location.
+//
+// Return true if it is possible to pick up and drop off all passengers for all the given trips, or false otherwise.
+//
+//
+//
+// Example 1:
+//
+// Input: trips = [[2,1,5],[3,3,7]], capacity = 4
+// Output: false
+//
+// Example 2:
+//
+// Input: trips = [[2,1,5],[3,3,7]], capacity = 5
+// Output: true
+//
+//
+//
+// Constraints:
+//
+// 	  * 1 <= trips.length <= 1000
+//
+// 	  * trips[i].length == 3
+//
+// 	  * 1 <= numPassengersi <= 100
+//
+// 	  * 0 <= fromi < toi <= 1000
+//
+// 	  * 1 <= capacity <= 105
+
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<pair<int,int>> vec;
+
+        for(auto i: trips){
+            vec.push_back({i[1],+i[0]});
+            vec.push_back({i[2],-i[0]});
+        }
+
+        sort(vec.begin(),vec.end());
+
+        for(int i = 1;i<vec.size();i++){
+            vec[i].second = vec[i].second + vec[i-1].second;
+
+            if(vec[i].second > capacity) return false;
+        }
+
+        return true;
+    }
+};
