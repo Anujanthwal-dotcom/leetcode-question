84a3f6cd8b680c5600d443eaddc17aa2ea2f5c53
Anuj Anthwal <152400521+Anujanthwal-dotcom@users.noreply.github.com>
2026-09-12 07:20:14 +0530

    Sync: 1770. Maximum Score from Performing Multiplication Operations (Hard) - Leet2Git

--- 1770_Maximum_Score_from_Performing_Multiplication_Operations.cpp
+++ 1770_Maximum_Score_from_Performing_Multiplication_Operations.cpp
@@ -72,8 +72,7 @@ public:
 private:
     int tryAllPossibilities(int i,int l,int r,int n,int m,vector<int>& nums, vector<int>& multipliers,vector<vector<int>>& dp){
         if(i>=m) return 0;
-        if(dp[l][r] != (int)1e8 ) return dp[l][r];
-
+        
         int chooseLeft = nums[l] + tryAllPossibilities(i+1,l+1,r,n,m,nums, multipliers, dp);
 
         int chooseRight = nums[r] + tryAllPossibilities(i+1,l,r-1,n,m,nums, multipliers, dp);
