adaa9cbd5b4a6ca0d93763c883835ad25ae2b377
Anuj Anthwal <152400521+Anujanthwal-dotcom@users.noreply.github.com>
2026-09-12 07:18:52 +0530

    Sync: 1770. Maximum Score from Performing Multiplication Operations (Hard) - Leet2Git

--- 1770_Maximum_Score_from_Performing_Multiplication_Operations.cpp
+++ 1770_Maximum_Score_from_Performing_Multiplication_Operations.cpp
@@ -65,14 +65,14 @@ public:
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
         int n=nums.size();
         int m = multipliers.size(); 
-        vector<vector<int>> dp(m+1,vector<int>(m+1,-1e9));
+        vector<vector<int>> dp(m+1,vector<int>(m+1,(int)-1e8));
         return tryAllPossibilities(0,0,n-1,n,m,nums,multipliers,dp);
     }
 
 private:
     int tryAllPossibilities(int i,int l,int r,int n,int m,vector<int>& nums, vector<int>& multipliers,vector<vector<int>>& dp){
         if(i>=m) return 0;
-        if(dp[l][r] != (int)1e9 ) return dp[l][r];
+        if(dp[l][r] != (int)1e8 ) return dp[l][r];
 
         int chooseLeft = nums[l] + tryAllPossibilities(i+1,l+1,r,n,m,nums, multipliers, dp);
 
