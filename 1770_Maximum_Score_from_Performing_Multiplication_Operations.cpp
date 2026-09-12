a25d0d365d021494387150694a1e55667031fefe
Anuj Anthwal <152400521+Anujanthwal-dotcom@users.noreply.github.com>
2026-09-12 07:15:09 +0530

    Sync: 1770. Maximum Score from Performing Multiplication Operations (Hard) - Leet2Git

---
+++ 1770_Maximum_Score_from_Performing_Multiplication_Operations.cpp
@@ -0,0 +1,84 @@
+// 1770. Maximum Score from Performing Multiplication Operations
+// Difficulty: Hard
+// URL: https://leetcode.com/problems/maximum-score-from-performing-multiplication-operations/
+//
+// You are given two 0-indexed integer arrays nums and multipliers of size n and m respectively, where n >= m.
+//
+// You begin with a score of 0. You want to perform exactly m operations. On the ith operation (0-indexed) you will:
+//
+//       * Choose one integer x from either the start or the end of the array nums.
+//
+//       * Add multipliers[i] * x to your score.
+//
+//           * Note that multipliers[0] corresponds to the first operation, multipliers[1] to the second operation, and so on.
+//
+//
+//
+//
+//       * Remove x from nums.
+//
+// Return the maximum score after performing m operations.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [1,2,3], multipliers = [3,2,1]
+// Output: 14
+// Explanation: An optimal solution is as follows:
+// - Choose from the end, [1,2,3], adding 3 * 3 = 9 to the score.
+// - Choose from the end, [1,2], adding 2 * 2 = 4 to the score.
+// - Choose from the end, [1], adding 1 * 1 = 1 to the score.
+// The total score is 9 + 4 + 1 = 14.
+//
+// Example 2:
+//
+// Input: nums = [-5,-3,-3,-2,7,1], multipliers = [-10,-5,3,4,6]
+// Output: 102
+// Explanation: An optimal solution is as follows:
+// - Choose from the start, [-5,-3,-3,-2,7,1], adding -5 * -10 = 50 to the score.
+// - Choose from the start, [-3,-3,-2,7,1], adding -3 * -5 = 15 to the score.
+// - Choose from the start, [-3,-2,7,1], adding -3 * 3 = -9 to the score.
+// - Choose from the end, [-2,7,1], adding 1 * 4 = 4 to the score.
+// - Choose from the end, [-2,7], adding 7 * 6 = 42 to the score. 
+// The total score is 50 + 15 - 9 + 4 + 42 = 102.
+//
+//
+//
+// Constraints:
+//
+// 	  * n == nums.length
+//
+// 	  * m == multipliers.length
+//
+// 	  * 1 <= m <= 300
+//
+// 	  * m <= n <= 105 
+//
+// 	  * -1000 <= nums[i], multipliers[i] <= 1000
+
+// recursion + memoization
+// tabulation is difficult here
+
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int n=nums.size();
+        int m = multipliers.size(); 
+        return tryAllPossibilities(0,0,n-1,n,m,nums,multipliers);
+    }
+
+private:
+    int dp[320][320] = {(int)-1e9};
+    
+    int tryAllPossibilities(int i,int l,int r,int n,int m,vector<int>& nums, vector<int>& multipliers){
+        if(i>=m) return 0;
+        if(dp[l][r] != (int)1e9 ) return dp[l][r];
+
+        int chooseLeft = nums[l] + tryAllPossibilities(i+1,l+1,r,n,m,nums, multipliers);
+
+        int chooseRight = nums[r] + tryAllPossibilities(i+1,l,r-1,n,m,nums, multipliers);
+
+        return max(chooseLeft, chooseRight);
+    }
+};
